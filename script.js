a7e3e2c3aaef3f2e1bea503843a1be4aeea0f74e
piaolivera <piaoliveraa@gmail.com>
2024-01-16 12:25:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
 const wordContainer = document.getElementById('wordContainer');
 const startButton = document.getElementById('startButton');
-const usedLettersElement = document.getElementById('usedLetters')
+const usedLettersElement = document.getElementById('usedLetters');
+
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+ctx.canvas.width = 0;
+ctx.canvas.height =0;
+
+
+const bodyParts = [
+    [4, 2, 1, 1],
+    [4, 3, 1, 2],
+    [3, 5, 1, 1],
+    [5, 5, 1, 1],
+    [3, 3, 1, 1],
+    [5, 3, 1, 1]
+];
+
+let selectedWord;
+let usedLetters;
+let mistakes;
+let hits;
+
+const drawHangMan = () =>{
+    ctx.canvas.width = 120;
+    ctx.canvas.height = 160;
+    ctx.scale(20, 20);
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    ctx.fillStyle = '#d95d39';
+    ctx.fillReact (0, 7, 4, 1);
+    ctx.fillReact (1, 0, 1, 8);
+    ctx.fillReact (2, 0, 3, 1);
+    ctx.fillReact (4, 1, 1, 1);
+};
+
+const startGame = () => {
+    usedLetters = [];
+    mistakes = 0;
+    wordContainer.innerHTML= '';
+    usedLettersElement.innerHTML = '';
+    startButton.style.display = 'none'; /*En el momento que damos play al juego, la palabra "start" desaparece*/
+};
+
+startButton.addEventListener('click', startGame);
