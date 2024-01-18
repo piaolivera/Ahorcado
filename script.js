65b5b92713c2ced3c64a72f3773873b3ba34903d
piaolivera <piaoliveraa@gmail.com>
2024-01-18 12:15:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -22,24 +22,44 @@ let usedLetters;
 let mistakes;
 let hits;
 
+const drawWord = () => {
+    selectedWord.forEach(letter => {
+        const letterElement = document.createElement('span');
+        letterElement.innerHTML = letter.toUpperCase();
+        letterElement.classList.add('letter');
+        usedLettersElement.classList.add('hidden');
+        wordContainer.appendChild(LetterElement);
+    });
+};
+
+const selectRandomWord = () => {
+    let word = words[Math.floor((Math.random() * words.length))].toUpperCase();
+    selectedWord = word.split('');
+};
+
 const drawHangMan = () =>{
     ctx.canvas.width = 120;
     ctx.canvas.height = 160;
     ctx.scale(20, 20);
     ctx.clearRect(0,0, canvas.width, canvas.height);
     ctx.fillStyle = '#d95d39';
-    ctx.fillReact (0, 7, 4, 1);
-    ctx.fillReact (1, 0, 1, 8);
-    ctx.fillReact (2, 0, 3, 1);
-    ctx.fillReact (4, 1, 1, 1);
+    ctx.fillRect (0, 7, 4, 1);
+    ctx.fillRect (1, 0, 1, 8);
+    ctx.fillRect (2, 0, 3, 1);
+    ctx.fillRect (4, 1, 1, 1);
 };
 
 const startGame = () => {
     usedLetters = [];
     mistakes = 0;
-    wordContainer.innerHTML= '';
+    hits = 0;
+    wordContainer.innerHTML = '';
     usedLettersElement.innerHTML = '';
-    startButton.style.display = 'none'; /*En el momento que damos play al juego, la palabra "start" desaparece*/
+    startButton.style.display = 'none';
+    drawHangMan(); /*Dibujamos al ahorcado*/ 
+    selectRandomWord();
+    drawWord();
+    document.addEventListener('keydown', letterEvent);
 };
 
 startButton.addEventListener('click', startGame);
